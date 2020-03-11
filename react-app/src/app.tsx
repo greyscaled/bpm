9b8e08287fc18dd0634f5fd1f6e9184b9aa44dee
Vapurrmaid <vapurrmaid@gmail.com>
2020-03-10 23:43:29 -0400

    Rm react-router

--- react-app/src/app.tsx
+++ react-app/src/app.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import { Layout } from "./components/layout";
 import { BPMCalculator } from "./components/bpm-calculator";
@@ -7,13 +6,7 @@ import { BPMCalculator } from "./components/bpm-calculator";
 export const App: React.FC = () => {
   return (
     <Layout>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <BPMCalculator />
-          </Route>
-        </Switch>
-      </Router>
+      <BPMCalculator />
     </Layout>
   );
 };
