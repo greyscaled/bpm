ec629af9fb0c6c0875e5a4578f4d2972cf94655d
Vapurrmaid <vapurrmaid@gmail.com>
2020-03-19 21:44:28 -0400

    Add layout css
    * improves overall style (especially header, footer fonts)
    * centers content on screens above 500px

--- react-app/src/components/layout.tsx
+++ react-app/src/components/layout.tsx
@@ -1,23 +1,25 @@
 import React from "react";
+import "./layout.css";
 
 export const Layout: React.FC = ({ children }) => (
   <>
-    <header>
+    <header className="header">
       <h1>Beats Calculator</h1>
     </header>
 
-    <main>
-      <article style={{ marginBottom: "20px" }}>
+    <main className="main">
+      <article className="article" style={{ marginBottom: "20px" }}>
         A digital metronome with extended beat and note length metrics.
       </article>
 
       {children}
     </main>
 
-    <footer style={{ marginTop: "40px" }}>
+    <footer className="footer" style={{ marginTop: "40px" }}>
       <p>
         {`\u00A9 ${new Date().getFullYear()}`}{" "}
         <a
+          className="link"
           href="https://github.com/vapurrmaid"
           rel="noreferrer noopener"
           target="_blank"
