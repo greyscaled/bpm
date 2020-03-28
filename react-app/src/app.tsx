87e32150ea4c44fb303dce52086de72ea2de204d
Vapurrmaid <vapurrmaid@gmail.com>
2020-03-27 21:33:52 -0400

    Add PWA manifest, images and service worker

--- react-app/src/app.tsx
+++ react-app/src/app.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Layout } from "./components/layout";
 import { BPMCalculator } from "./components/bpm-calculator";
 
+export interface PWAEvent extends Event {
+  prompt(): Promise<void>;
+}
+
 export const App: React.FC = () => {
+  const [pwaEvt, setPwaEvt] = useState<PWAEvent | undefined>(undefined);
+
+  useEffect(() => {
+    const beforePWAInstall = (event: Event) => {
+      event.preventDefault();
+      setPwaEvt(event as PWAEvent);
+    };
+    window.addEventListener("beforeinstallprompt", beforePWAInstall);
+
+    const afterPWAInstall = () => {
+      setPwaEvt(undefined);
+    };
+    window.addEventListener("appinstalled", afterPWAInstall);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", beforePWAInstall);
+      window.removeEventListener("appinstalled", afterPWAInstall);
+    };
+  }, []);
+
   return (
-    <Layout>
+    <Layout pwaEvt={pwaEvt}>
       <BPMCalculator />
     </Layout>
   );
