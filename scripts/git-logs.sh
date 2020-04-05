ccb342851605e956b7f310ed8908249e8fcdf35f
Vapurrmaid <vapurrmaid@gmail.com>
2020-04-05 15:55:41 -0400

    Add git-log script
    This script pretty prints information about git logs for a particular
    sub-package within this monorepo.
    
    For exampple, running git-log react-app will show the latest commit, tag
    and changelog (or difflog) for commits only made within the react-app
    directory

---
+++ scripts/git-logs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+function usage() {
+  echo "Usage: git-logs <lib | react-app> [--changelog | --difflog]"
+}
+
+pushd $(git rev-parse --show-toplevel)
+  if [ "${1:-}" == "lib" ]; then
+    LATEST_COMMIT=$(git log -1 --pretty=format:"%h" lib)
+  elif [ "${1:-}" == "react-app" ]; then
+    LATEST_COMMIT=$(git log -1 --pretty=format:"%h" react-app)
+  else
+    echo "Unknown argument"
+    usage
+    exit 1 
+  fi
+
+  LATEST_TAG=$(git describe --abbrev=0 $LATEST_COMMIT)
+
+  echo "Latest commit:    $LATEST_COMMIT"
+  echo "Latest tag   :    $LATEST_TAG"
+
+  if [ "${2:-}" == "--changelog" ] || [ "${2:-}" == "" ]; then
+    echo "Changelog    :"
+    git log --pretty=format:"  * %s (%h)" $LATEST_TAG...HEAD $1
+  elif [ "${2:-}" == "--difflog" ]; then
+    echo "Difflog      :"
+    git diff HEAD $LATEST_TAG $1
+  else
+    echo "Unknown option $2"
+    usage
+    exit 1
+  fi
+popd
