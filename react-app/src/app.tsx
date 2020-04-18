c5ed91dd27fa50c42459354a1fccf6652c78544c
Vapurrmaid <vapurrmaid@gmail.com>
2020-04-17 20:59:17 -0400

    Create clicktrack context
    This is a first phase in refactoring clicktrack functionality.
    Eventually, the entirety of the clicktrack class should migrate to the
    ClickTrackProvider React.FC. However, we'll cross that bridge separately
    as it's a larger refactor. For now, this yields an improved singleton
    interface to ToneJS's globals.

--- react-app/src/app.tsx
+++ react-app/src/app.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import { Layout } from "./components/layout";
 import { BPMCalculator } from "./components/bpm-calculator";
+import { ClickTrackProvider } from "./contexts/clicktrack";
 import { PWAProvider } from "./contexts/pwa";
 
 export const App: React.FC = () => (
   <PWAProvider>
     <Layout>
-      <BPMCalculator />
+      <ClickTrackProvider>
+        <BPMCalculator />
+      </ClickTrackProvider>
     </Layout>
   </PWAProvider>
 );
---
+++ react-app/src/contexts/clicktrack.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useRef } from "react";
+import { ClickTrack } from "../util/clicktrack";
+
+/** Singleton ClickTrack */
+const clickTrack = new ClickTrack();
+
+const ClickTrackContext = createContext<ClickTrack>(clickTrack);
+
+export const useClickTrackRef = () => {
+  const context = useContext(ClickTrackContext);
+  return useRef(context);
+};
+
+export const ClickTrackProvider: React.FC = ({ children }) => {
+  return (
+    <ClickTrackContext.Provider value={clickTrack}>
+      {children}
+    </ClickTrackContext.Provider>
+  );
+};
