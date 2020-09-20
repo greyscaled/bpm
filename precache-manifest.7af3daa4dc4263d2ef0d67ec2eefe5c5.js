95f28c08c2d14ed22bc54e04c61a5e7e13a11cae
vapurrmaid <vapurrmaid@users.noreply.github.com>
2020-09-20 16:30:59 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.7af3daa4dc4263d2ef0d67ec2eefe5c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db77214ab51402bce38955ba33ff65e0",
+    "url": "/bpm/index.html"
+  },
+  {
+    "revision": "3787515e1fb1b057115e",
+    "url": "/bpm/static/css/main.d2e69d45.chunk.css"
+  },
+  {
+    "revision": "bbc723168214c56af23d",
+    "url": "/bpm/static/js/2.b4d63563.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "/bpm/static/js/2.b4d63563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3787515e1fb1b057115e",
+    "url": "/bpm/static/js/main.a574cc21.chunk.js"
+  },
+  {
+    "revision": "f2ba6b59796762ef2408",
+    "url": "/bpm/static/js/runtime-main.9e80904e.js"
+  },
+  {
+    "revision": "b6a173b64c898a0682a8c29dcaca8b16",
+    "url": "/bpm/static/media/arrow.b6a173b6.svg"
+  },
+  {
+    "revision": "ee30c7da38942e76d3b9eba3d4a07f5a",
+    "url": "/bpm/static/media/eighth.ee30c7da.svg"
+  },
+  {
+    "revision": "6d02873b0ad58fc6f9b5b8c1f2e5f8e8",
+    "url": "/bpm/static/media/half.6d02873b.svg"
+  },
+  {
+    "revision": "b6a173b64c898a0682a8c29dcaca8b16",
+    "url": "/bpm/static/media/play.b6a173b6.svg"
+  },
+  {
+    "revision": "2eac25c9ec58f58608ecfe1392a97e42",
+    "url": "/bpm/static/media/quarter.2eac25c9.svg"
+  },
+  {
+    "revision": "2f87e06c1d7cb250255bacc8a19ba341",
+    "url": "/bpm/static/media/sixteenth.2f87e06c.svg"
+  },
+  {
+    "revision": "204e4507201a012dc7b63aa9e2beb7af",
+    "url": "/bpm/static/media/stop.204e4507.svg"
+  },
+  {
+    "revision": "da291ffb166fac56c54e0f8441ef0052",
+    "url": "/bpm/static/media/whole.da291ffb.svg"
+  }
+]);
