762f5cbc0b8ed98a88887f73509b662758f9f39f
G r e y <grey@coder.com>
2020-09-20 11:22:48 -0500

    Update release scripts

--- scripts/release-lib.sh
+++ scripts/release-lib.sh
@@ -5,7 +5,8 @@
 set -euox pipefail
 
 pushd $(git rev-parse --show-toplevel)
-./scripts/git-logs.sh lib
+  ./scripts/git-logs.sh lib
   cd lib
+  yarn build
   ../node_modules/.bin/release-it --config ../.release-it-lib.json
 popd
--- scripts/release-react.sh
+++ scripts/release-react.sh
@@ -7,5 +7,6 @@ set -euox pipefail
 pushd $(git rev-parse --show-toplevel)
   ./scripts/git-logs.sh react-app
   cd react-app
+  yarn build
   ../node_modules/.bin/release-it --config ../.release-it-react.json
 popd
